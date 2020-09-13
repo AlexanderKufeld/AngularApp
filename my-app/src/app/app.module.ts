68ed9097aeaf9f0b265ad3337436a98cb3006f35
AlexanderKufeld <alexander.kufeld@gmail.com>
2020-09-13 16:50:43 +0200

    project updated

---
+++ my-app/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import { UserComponentComponent } from './user-component/user-component.component';
+import { FilterContainerComponent } from './filter-container/filter-container.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserComponentComponent,
+    FilterContainerComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
